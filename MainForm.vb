1f3afe5457e012bdc16110cd63b3418aa23c73d3
eMonkey <genialodotcom@gmail.com>
2024-04-30 16:56:50 -0600

    x

--- MainForm.vb
+++ MainForm.vb
@@ -45,7 +45,4 @@ Public Class MainForm
         FilteredFileListBox.Items.AddRange(fileList.ToArray())
     End Sub
 
-    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
