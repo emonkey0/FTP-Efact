8d392150e9b9824dd7ab261044106494b5b2f78a
eMonkey <genialodotcom@gmail.com>
2024-04-30 16:23:44 -0600

    okay lest gou

---
+++ MainForm.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ConnectButton = New System.Windows.Forms.Button()
+        Me.FtpAddressTextBox = New System.Windows.Forms.TextBox()
+        Me.FileListBox = New System.Windows.Forms.ListBox()
+        Me.SearchTextBox = New System.Windows.Forms.TextBox()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.FilteredFileListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'ConnectButton
+        '
+        Me.ConnectButton.Location = New System.Drawing.Point(353, 220)
+        Me.ConnectButton.Name = "ConnectButton"
+        Me.ConnectButton.Size = New System.Drawing.Size(75, 23)
+        Me.ConnectButton.TabIndex = 0
+        Me.ConnectButton.Text = "Connect"
+        Me.ConnectButton.UseVisualStyleBackColor = True
+        '
+        'FtpAddressTextBox
+        '
+        Me.FtpAddressTextBox.Location = New System.Drawing.Point(340, 194)
+        Me.FtpAddressTextBox.Name = "FtpAddressTextBox"
+        Me.FtpAddressTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.FtpAddressTextBox.TabIndex = 1
+        '
+        'FileListBox
+        '
+        Me.FileListBox.FormattingEnabled = True
+        Me.FileListBox.Location = New System.Drawing.Point(467, 129)
+        Me.FileListBox.Name = "FileListBox"
+        Me.FileListBox.Size = New System.Drawing.Size(120, 95)
+        Me.FileListBox.TabIndex = 2
+        '
+        'SearchTextBox
+        '
+        Me.SearchTextBox.Location = New System.Drawing.Point(594, 12)
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.SearchTextBox.TabIndex = 3
+        Me.SearchTextBox.Text = "SearchTextBox"
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(604, 38)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(75, 23)
+        Me.SearchButton.TabIndex = 4
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'FilteredFileListBox
+        '
+        Me.FilteredFileListBox.FormattingEnabled = True
+        Me.FilteredFileListBox.Location = New System.Drawing.Point(638, 129)
+        Me.FilteredFileListBox.Name = "FilteredFileListBox"
+        Me.FilteredFileListBox.Size = New System.Drawing.Size(120, 95)
+        Me.FilteredFileListBox.TabIndex = 5
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FilteredFileListBox)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.SearchTextBox)
+        Me.Controls.Add(Me.FileListBox)
+        Me.Controls.Add(Me.FtpAddressTextBox)
+        Me.Controls.Add(Me.ConnectButton)
+        Me.Name = "MainForm"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ConnectButton As Windows.Forms.Button
+    Friend WithEvents FtpAddressTextBox As Windows.Forms.TextBox
+    Friend WithEvents FileListBox As Windows.Forms.ListBox
+    Friend WithEvents SearchTextBox As Windows.Forms.TextBox
+    Friend WithEvents SearchButton As Windows.Forms.Button
+    Friend WithEvents FilteredFileListBox As Windows.Forms.ListBox
+End Class
---
+++ MainForm.vb
@@ -0,0 +1,47 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Windows.Forms
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+
+Public Class MainForm
+    Private Sub ConnectButton_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
+        ' Establish FTP connection
+        Dim ftpAddress As String = FtpAddressTextBox.Text
+        Dim username As String = "android"
+        Dim password As String = "android"
+
+        Dim request As FtpWebRequest = DirectCast(WebRequest.Create(ftpAddress), FtpWebRequest)
+        request.Method = WebRequestMethods.Ftp.ListDirectoryDetails
+        request.Credentials = New NetworkCredential(username, password)
+
+        Try
+            Dim response As FtpWebResponse = DirectCast(request.GetResponse(), FtpWebResponse)
+            Dim responseStream As Stream = response.GetResponseStream()
+            Dim reader As New StreamReader(responseStream)
+            Dim directoryContent As String = reader.ReadToEnd()
+            response.Close()
+
+            ' Display directory content in ListBox
+            FileListBox.Items.Clear()
+            FileListBox.Items.AddRange(directoryContent.Split(vbCrLf.ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+        Catch ex As Exception
+            MessageBox.Show("Error connecting to FTP server: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        Dim searchText As String = SearchTextBox.Text
+        Dim fileList As New List(Of String)
+
+        ' Filter files based on search text
+        For Each item As String In FileListBox.Items
+            If item.Contains(searchText) Then
+                fileList.Add(item)
+            End If
+        Next
+
+        ' Display filtered files in ListBox
+        FilteredFileListBox.Items.Clear()
+        FilteredFileListBox.Items.AddRange(fileList.ToArray())
+    End Sub
+End Class
