350c627c05c25637fd719e51c87cb69757579266
eMonkey <genialodotcom@gmail.com>
2024-05-02 15:41:31 -0600

    Summary

--- MainForm.Designer.vb
+++ MainForm.Designer.vb
@@ -22,72 +22,138 @@ Partial Class MainForm
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.ConnectButton = New System.Windows.Forms.Button()
-        Me.FtpAddressTextBox = New System.Windows.Forms.TextBox()
-        Me.FileListBox = New System.Windows.Forms.ListBox()
-        Me.SearchTextBox = New System.Windows.Forms.TextBox()
-        Me.SearchButton = New System.Windows.Forms.Button()
-        Me.FilteredFileListBox = New System.Windows.Forms.ListBox()
-        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.SELECCIONAR = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.RFC = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MES = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AÑO = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.UUID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DESCARGAR = New System.Windows.Forms.DataGridViewButtonColumn()
+        Me.StartDateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.EndDateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.Buscar = New System.Windows.Forms.Button()
+        Me.RFCTextBox = New System.Windows.Forms.TextBox()
+        Me.UUIDTextBox = New System.Windows.Forms.TextBox()
+        Me.DownloadButton = New System.Windows.Forms.Button()
+        Me.RFCLabel = New System.Windows.Forms.Label()
+        Me.UUIDLabel = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'ConnectButton
+        'DataGridView1
         '
-        Me.ConnectButton.Location = New System.Drawing.Point(353, 220)
-        Me.ConnectButton.Name = "ConnectButton"
-        Me.ConnectButton.Size = New System.Drawing.Size(75, 23)
-        Me.ConnectButton.TabIndex = 0
-        Me.ConnectButton.Text = "Connect"
-        Me.ConnectButton.UseVisualStyleBackColor = True
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.SELECCIONAR, Me.RFC, Me.MES, Me.AÑO, Me.UUID, Me.DESCARGAR})
+        Me.DataGridView1.Location = New System.Drawing.Point(32, 120)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(678, 198)
+        Me.DataGridView1.TabIndex = 5
         '
-        'FtpAddressTextBox
+        'SELECCIONAR
         '
-        Me.FtpAddressTextBox.Location = New System.Drawing.Point(340, 194)
-        Me.FtpAddressTextBox.Name = "FtpAddressTextBox"
-        Me.FtpAddressTextBox.Size = New System.Drawing.Size(100, 20)
-        Me.FtpAddressTextBox.TabIndex = 1
+        Me.SELECCIONAR.HeaderText = "SELECCIONAR"
+        Me.SELECCIONAR.Name = "SELECCIONAR"
+        Me.SELECCIONAR.ReadOnly = True
         '
-        'FileListBox
+        'RFC
         '
-        Me.FileListBox.FormattingEnabled = True
-        Me.FileListBox.Location = New System.Drawing.Point(467, 129)
-        Me.FileListBox.Name = "FileListBox"
-        Me.FileListBox.Size = New System.Drawing.Size(120, 95)
-        Me.FileListBox.TabIndex = 2
+        Me.RFC.HeaderText = "RFC"
+        Me.RFC.Name = "RFC"
+        Me.RFC.ReadOnly = True
         '
-        'SearchTextBox
+        'MES
         '
-        Me.SearchTextBox.Location = New System.Drawing.Point(594, 12)
-        Me.SearchTextBox.Name = "SearchTextBox"
-        Me.SearchTextBox.Size = New System.Drawing.Size(100, 20)
-        Me.SearchTextBox.TabIndex = 3
-        Me.SearchTextBox.Text = "SearchTextBox"
+        Me.MES.HeaderText = "MES"
+        Me.MES.Name = "MES"
+        Me.MES.ReadOnly = True
         '
-        'SearchButton
+        'AÑO
         '
-        Me.SearchButton.Location = New System.Drawing.Point(604, 38)
-        Me.SearchButton.Name = "SearchButton"
-        Me.SearchButton.Size = New System.Drawing.Size(75, 23)
-        Me.SearchButton.TabIndex = 4
-        Me.SearchButton.Text = "Search"
-        Me.SearchButton.UseVisualStyleBackColor = True
+        Me.AÑO.HeaderText = "AÑO"
+        Me.AÑO.Name = "AÑO"
+        Me.AÑO.ReadOnly = True
         '
-        'FilteredFileListBox
+        'UUID
         '
-        Me.FilteredFileListBox.FormattingEnabled = True
-        Me.FilteredFileListBox.Location = New System.Drawing.Point(638, 129)
-        Me.FilteredFileListBox.Name = "FilteredFileListBox"
-        Me.FilteredFileListBox.Size = New System.Drawing.Size(120, 95)
-        Me.FilteredFileListBox.TabIndex = 5
+        Me.UUID.HeaderText = "UUID"
+        Me.UUID.Name = "UUID"
+        Me.UUID.ReadOnly = True
         '
-        'CheckedListBox1
+        'DESCARGAR
         '
-        Me.CheckedListBox1.FormattingEnabled = True
-        Me.CheckedListBox1.Location = New System.Drawing.Point(340, 279)
-        Me.CheckedListBox1.MultiColumn = True
-        Me.CheckedListBox1.Name = "CheckedListBox1"
-        Me.CheckedListBox1.Size = New System.Drawing.Size(212, 94)
-        Me.CheckedListBox1.TabIndex = 6
+        Me.DESCARGAR.HeaderText = "DESCARGAR"
+        Me.DESCARGAR.Name = "DESCARGAR"
+        Me.DESCARGAR.ReadOnly = True
+        Me.DESCARGAR.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DESCARGAR.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
+        '
+        'StartDateTimePicker
+        '
+        Me.StartDateTimePicker.DropDownAlign = System.Windows.Forms.LeftRightAlignment.Right
+        Me.StartDateTimePicker.Location = New System.Drawing.Point(32, 33)
+        Me.StartDateTimePicker.Name = "StartDateTimePicker"
+        Me.StartDateTimePicker.Size = New System.Drawing.Size(200, 20)
+        Me.StartDateTimePicker.TabIndex = 6
+        '
+        'EndDateTimePicker
+        '
+        Me.EndDateTimePicker.Location = New System.Drawing.Point(270, 33)
+        Me.EndDateTimePicker.Name = "EndDateTimePicker"
+        Me.EndDateTimePicker.Size = New System.Drawing.Size(200, 20)
+        Me.EndDateTimePicker.TabIndex = 7
+        '
+        'Buscar
+        '
+        Me.Buscar.Location = New System.Drawing.Point(614, 30)
+        Me.Buscar.Name = "Buscar"
+        Me.Buscar.Size = New System.Drawing.Size(75, 23)
+        Me.Buscar.TabIndex = 8
+        Me.Buscar.Text = "Buscar"
+        Me.Buscar.UseVisualStyleBackColor = True
+        '
+        'RFCTextBox
+        '
+        Me.RFCTextBox.Location = New System.Drawing.Point(497, 32)
+        Me.RFCTextBox.Name = "RFCTextBox"
+        Me.RFCTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.RFCTextBox.TabIndex = 9
+        '
+        'UUIDTextBox
+        '
+        Me.UUIDTextBox.Location = New System.Drawing.Point(497, 78)
+        Me.UUIDTextBox.Name = "UUIDTextBox"
+        Me.UUIDTextBox.Size = New System.Drawing.Size(100, 20)
+        Me.UUIDTextBox.TabIndex = 10
+        '
+        'DownloadButton
+        '
+        Me.DownloadButton.Location = New System.Drawing.Point(435, 345)
+        Me.DownloadButton.Name = "DownloadButton"
+        Me.DownloadButton.Size = New System.Drawing.Size(75, 23)
+        Me.DownloadButton.TabIndex = 11
+        Me.DownloadButton.Text = "Button2"
+        Me.DownloadButton.UseVisualStyleBackColor = True
+        '
+        'RFCLabel
+        '
+        Me.RFCLabel.AutoSize = True
+        Me.RFCLabel.Location = New System.Drawing.Point(524, 9)
+        Me.RFCLabel.Name = "RFCLabel"
+        Me.RFCLabel.Size = New System.Drawing.Size(28, 13)
+        Me.RFCLabel.TabIndex = 12
+        Me.RFCLabel.Text = "RFC"
+        '
+        'UUIDLabel
+        '
+        Me.UUIDLabel.AutoSize = True
+        Me.UUIDLabel.Location = New System.Drawing.Point(524, 62)
+        Me.UUIDLabel.Name = "UUIDLabel"
+        Me.UUIDLabel.Size = New System.Drawing.Size(34, 13)
+        Me.UUIDLabel.TabIndex = 13
+        Me.UUIDLabel.Text = "UUID"
         '
         'MainForm
         '
@@ -95,25 +161,44 @@ Partial Class MainForm
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveBorder
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.CheckedListBox1)
-        Me.Controls.Add(Me.FilteredFileListBox)
-        Me.Controls.Add(Me.SearchButton)
-        Me.Controls.Add(Me.SearchTextBox)
-        Me.Controls.Add(Me.FileListBox)
-        Me.Controls.Add(Me.FtpAddressTextBox)
-        Me.Controls.Add(Me.ConnectButton)
+        Me.Controls.Add(Me.UUIDLabel)
+        Me.Controls.Add(Me.RFCLabel)
+        Me.Controls.Add(Me.DownloadButton)
+        Me.Controls.Add(Me.UUIDTextBox)
+        Me.Controls.Add(Me.RFCTextBox)
+        Me.Controls.Add(Me.Buscar)
+        Me.Controls.Add(Me.EndDateTimePicker)
+        Me.Controls.Add(Me.StartDateTimePicker)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Name = "MainForm"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
+    Friend WithEvents DataGridView1 As Windows.Forms.DataGridView
+    Friend WithEvents StartDateTimePicker As Windows.Forms.DateTimePicker
 
-    Friend WithEvents ConnectButton As Windows.Forms.Button
-    Friend WithEvents FtpAddressTextBox As Windows.Forms.TextBox
-    Friend WithEvents FileListBox As Windows.Forms.ListBox
-    Friend WithEvents SearchTextBox As Windows.Forms.TextBox
-    Friend WithEvents SearchButton As Windows.Forms.Button
-    Friend WithEvents FilteredFileListBox As Windows.Forms.ListBox
-    Friend WithEvents CheckedListBox1 As Windows.Forms.CheckedListBox
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Friend WithEvents SELECCIONAR As Windows.Forms.DataGridViewCheckBoxColumn
+    Friend WithEvents RFC As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents MES As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents AÑO As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents UUID As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DESCARGAR As Windows.Forms.DataGridViewButtonColumn
+    Friend WithEvents EndDateTimePicker As Windows.Forms.DateTimePicker
+    Friend WithEvents Buscar As Windows.Forms.Button
+    Friend WithEvents RFCTextBox As Windows.Forms.TextBox
+    Friend WithEvents UUIDTextBox As Windows.Forms.TextBox
+    Friend WithEvents DownloadButton As Windows.Forms.Button
+    Friend WithEvents RFCLabel As Windows.Forms.Label
+    Friend WithEvents UUIDLabel As Windows.Forms.Label
 End Class
--- MainForm.vb
+++ MainForm.vb
@@ -1,48 +1,194 @@
-﻿Imports System.IO
-Imports System.Net
+﻿Imports System.Net
+Imports System.IO
 Imports System.Windows.Forms
-Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
 
 Public Class MainForm
-    Private Sub ConnectButton_Click(sender As Object, e As EventArgs) Handles ConnectButton.Click
-        ' Establish FTP connection
-        Dim ftpAddress As String = FtpAddressTextBox.Text
-        Dim username As String = "android"
-        Dim password As String = "android"
+    Private ftpServer As String = "ftp://192.168.1.12:2221"
+    Private ftpUsername As String = "android"
+    Private ftpPassword As String = "android"
 
-        Dim request As FtpWebRequest = DirectCast(WebRequest.Create(ftpAddress), FtpWebRequest)
-        request.Method = WebRequestMethods.Ftp.ListDirectoryDetails
-        request.Credentials = New NetworkCredential(username, password)
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Configuración inicial del formulario
+        fechaI_txt.MaxDate = New DateTime(Now.Year, 12, 31)
+        fechaF.MaxDate = New DateTime(Now.Year, 12, 31)
+        fechaF.MinDate = New DateTime(Now.Year, 1, 1)
+        fechaI_txt.Value = New DateTime(Now.Year, Now.Month, 1)
+        fechaF.Value = New DateTime(Now.Year, Now.Month, DateTime.DaysInMonth(Now.Year, Now.Month))
+    End Sub
 
+    Private Sub Buscar_Click(sender As Object, e As EventArgs) Handles Buscar.Click
+        Dim rfc As String = rfc_txt.Text
+        Dim startDate As DateTime = fechaI_txt.Value
+        Dim endDate As DateTime = fechaF.Value
+
+        ' Limpiar DataGridView antes de realizar una nueva búsqueda
+        DataGridView1.Rows.Clear()
+
+        ' Llamar a los métodos de búsqueda según los parámetros proporcionados
+        If String.IsNullOrWhiteSpace(rfc) Then
+            ' Búsqueda por rango de fechas
+            SearchByDate(startDate, endDate)
+        Else
+            ' Búsqueda por RFC y rango de fechas
+            SearchByRFC(rfc, startDate, endDate)
+        End If
+    End Sub
+
+    Private Sub SearchByDate(startDate As DateTime, endDate As DateTime)
         Try
-            Dim response As FtpWebResponse = DirectCast(request.GetResponse(), FtpWebResponse)
-            Dim responseStream As Stream = response.GetResponseStream()
-            Dim reader As New StreamReader(responseStream)
-            Dim directoryContent As String = reader.ReadToEnd()
-            response.Close()
+            Dim request As FtpWebRequest = DirectCast(WebRequest.Create(ftpServer), FtpWebRequest)
+            request.Method = WebRequestMethods.Ftp.ListDirectory
+            request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
 
-            ' Display directory content in ListBox
-            FileListBox.Items.Clear()
-            FileListBox.Items.AddRange(directoryContent.Split(vbCrLf.ToCharArray(), StringSplitOptions.RemoveEmptyEntries))
+            Using response As FtpWebResponse = DirectCast(request.GetResponse(), FtpWebResponse)
+                Using stream As Stream = response.GetResponseStream()
+                    Using reader As New StreamReader(stream)
+                        Dim line As String = reader.ReadLine()
+                        While line IsNot Nothing
+                            If IsDateFolderInRange(line, startDate, endDate) Then
+                                Dim directory As String = line
+                                Dim files As List(Of String) = GetXmlFilesInDirectory(directory)
+                                AddFilesToDataGridView(files, directory)
+                            End If
+                            line = reader.ReadLine()
+                        End While
+                    End Using
+                End Using
+            End Using
         Catch ex As Exception
-            MessageBox.Show("Error connecting to FTP server: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Error al realizar la búsqueda por fecha: " & ex.Message)
         End Try
     End Sub
 
-    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
-        Dim searchText As String = SearchTextBox.Text
-        Dim fileList As New List(Of String)
+    Private Function IsDateFolderInRange(folderName As String, startDate As DateTime, endDate As DateTime) As Boolean
+        Dim folderDate As DateTime
+        If DateTime.TryParse(folderName, folderDate) Then
+            Return folderDate >= startDate AndAlso folderDate <= endDate
+        End If
+        Return False
+    End Function
 
-        ' Filter files based on search text
-        For Each item As String In FileListBox.Items
-            If item.Contains(searchText) Then
-                fileList.Add(item)
-            End If
+    Private Function GetXmlFilesInDirectory(directory As String) As List(Of String)
+        Dim files As New List(Of String)()
+
+        Try
+            Dim request As FtpWebRequest = DirectCast(WebRequest.Create($"{ftpServer}/{directory}"), FtpWebRequest)
+            request.Method = WebRequestMethods.Ftp.ListDirectory
+            request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+            Using response As FtpWebResponse = DirectCast(request.GetResponse(), FtpWebResponse)
+                Using stream As Stream = response.GetResponseStream()
+                    Using reader As New StreamReader(stream)
+                        Dim line As String = reader.ReadLine()
+                        While line IsNot Nothing
+                            If line.EndsWith(".xml") Then
+                                files.Add($"{ftpServer}/{directory}/{line}")
+                            End If
+                            line = reader.ReadLine()
+                        End While
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"Error al obtener archivos .xml en el directorio {directory}: {ex.Message}")
+        End Try
+
+        Return files
+    End Function
+
+    Private Sub AddFilesToDataGridView(files As List(Of String), directory As String)
+        For Each file In files
+            Dim fileName As String = Path.GetFileName(file)
+            Dim row As String() = New String() {directory, fileName}
+            DataGridView1.Rows.Add(row)
         Next
-
-        ' Display filtered files in ListBox
-        FilteredFileListBox.Items.Clear()
-        FilteredFileListBox.Items.AddRange(fileList.ToArray())
     End Sub
 
+    Private Sub SearchByRFC(rfc As String, startDate As DateTime, endDate As DateTime)
+        ' Búsqueda por RFC y rango de fechas
+        Try
+            Dim request As FtpWebRequest = DirectCast(WebRequest.Create(ftpServer), FtpWebRequest)
+            request.Method = WebRequestMethods.Ftp.ListDirectory
+            request.Credentials = New NetworkCredential(ftpUsername, ftpPassword)
+
+            Using response As FtpWebResponse = DirectCast(request.GetResponse(), FtpWebResponse)
+                Using stream As Stream = response.GetResponseStream()
+                    Using reader As New StreamReader(stream)
+                        Dim line As String = reader.ReadLine()
+                        While line IsNot Nothing
+                            Dim directory As String = line
+                            If directory.StartsWith(rfc) AndAlso IsDateFolderInRange(directory.Substring(rfc.Length + 1), startDate, endDate) Then
+                                Dim files As List(Of String) = GetXmlFilesInDirectory(directory)
+                                AddFilesToDataGridView(files, directory)
+                            End If
+                            line = reader.ReadLine()
+                        End While
+                    End Using
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error al realizar la búsqueda por RFC: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Try
+            Dim senderGrid = DirectCast(sender, DataGridView)
+            If TypeOf senderGrid.Columns(e.ColumnIndex) Is DataGridViewButtonColumn AndAlso
+               e.RowIndex >= 0 Then
+                Dim x = New FolderBrowserDialog
+                x.ShowDialog()
+                'Dim coneccionFTP = "ftp://ftp.site4now.net/webService/XML_GESEM/" & año & "/" & mes & "/" & rfc & "/" & DataGridView1.Rows(e.RowIndex).Cells("UUID").Value
+                Dim coneccionFTP = DataGridView1.Rows(e.RowIndex).Cells("ruta").Value
+                My.Computer.Network.DownloadFile(coneccionFTP, x.SelectedPath & "/" & DataGridView1.Rows(e.RowIndex).Cells("UUID").Value, "efactweb-002", "8e344aFea4a21825")
+                MessageBox.Show("DESCARGA FINALIZADA")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub descargarTodo_btn_Click(sender As Object, e As EventArgs) Handles descargarTodo_btn.Click
+        Try
+            Dim dialog = New FolderBrowserDialog
+            dialog.ShowDialog()
+            Dim folder = dialog.SelectedPath
+            For Each row As DataGridViewRow In DataGridView1.Rows
+                My.Computer.Network.DownloadFile(row.Cells("ruta").Value, folder & "/" & row.Cells("UUID").Value, "efactweb-002", "8e344aFea4a21825")
+            Next
+            MessageBox.Show("DESCARGA FINALIZADA")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Public Function MES(mesNum As Integer) As String
+        Select Case mesNum
+            Case 1
+                Return "ENERO"
+            Case 2
+                Return "FEBRERO"
+            Case 3
+                Return "MARZO"
+            Case 4
+                Return "ABRIL"
+            Case 5
+                Return "MAYO"
+            Case 6
+                Return "JUNIO"
+            Case 7
+                Return "JULIO"
+            Case 8
+                Return "AGOSTO"
+            Case 9
+                Return "SEPTIEMBRE"
+            Case 10
+                Return "OCTUBRE"
+            Case 11
+                Return "NOVIEMBRE"
+            Case 12
+                Return "DICIEMBRE"
+        End Select
+        Return ""
+    End Function
 End Class
